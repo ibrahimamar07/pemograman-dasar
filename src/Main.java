6ccc57c8d846cad96498e74dbb788831d6357a7d
ibrahimamar07 <ibrahimamaralfanani07@gmail.com>
2024-03-08 08:07:50 +0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args ){
         System.out.println("hello world");
-
         System.out.println("   .--.   ");
         System.out.println("   ' |\\/'   ");
-        System.out.printf(" ");
-        System.out.printf(" ");
-        System.out.printf(" ");
-        System.out.printf(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
 
     }
 }
